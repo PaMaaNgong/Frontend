e7501640d6b4d565329de4b5cb7cad00a70dfe9f
Natty_UniWorky <nnapatsiri@gmail.com>
2024-01-06 14:54:41 +0700

    i love pee

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar';
-import SearchResultsList from './components/SearchResultsList';
-<<<<<<< HEAD
-import Navbar from './components/Navbar';
-import Search from './components/Search'
-=======
-// import Navbar from './components/Navbar';
->>>>>>> eeb0879a8e8c4dc8c531ae99e0badfe8e2a29db7
+import Navbar from './components/Homepage/Navbar';
+import SearchBar from './components/Homepage/Searchbar';
+
 
 
 const App: React.FC = () => {
@@ -15,17 +10,8 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Navbar />
-      <Search />
-      {/* <div className="search-bar-container">
-=======
-      {/*<Navbar />*/}
-      <div className="search-bar-container">
->>>>>>> eeb0879a8e8c4dc8c531ae99e0badfe8e2a29db7
-        <SearchBar setResults={setResults} />
-        {results && results.length > 0 && <SearchResultsList results={results} />}
-      </div> */}
+      <Navbar/>
+      <SearchBar />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-=======
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
->>>>>>> eeb0879a8e8c4dc8c531ae99e0badfe8e2a29db7
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
 }
 
-=======
-}
->>>>>>> eeb0879a8e8c4dc8c531ae99e0badfe8e2a29db7
