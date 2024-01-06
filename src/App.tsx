be51fb728d1e1897487de7c0c320324b57b181df
Panyapat Wongdee <panyapat12345@gmail.com>
2024-01-06 14:53:02 +0700

    fix packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,23 @@
-import React, { useState } from 'react';
-import './App.css';
-import SearchBar from './components/SearchBar';
-import SearchResultsList from './components/SearchResultsList';
-<<<<<<< HEAD
-import Navbar from './components/Navbar';
-import Search from './components/Search'
-=======
-// import Navbar from './components/Navbar';
->>>>>>> eeb0879a8e8c4dc8c531ae99e0badfe8e2a29db7
-
+import React, { useState } from "react";
+import "./App.css";
+import SearchBar from "./components/SearchBar";
+import SearchResultsList from "./components/SearchResultsList";
+import Navbar from "./components/Navbar";
+import Search from "./components/Search";
+import Review from "./components/Reviews/Review";
 
 const App: React.FC = () => {
-  const [results, setResults] = useState<any[]>([]); 
+  const [results, setResults] = useState<any[]>([]);
 
   return (
     <div className="App">
-<<<<<<< HEAD
       <Navbar />
-      <Search />
-      {/* <div className="search-bar-container">
-=======
-      {/*<Navbar />*/}
       <div className="search-bar-container">
->>>>>>> eeb0879a8e8c4dc8c531ae99e0badfe8e2a29db7
         <SearchBar setResults={setResults} />
-        {results && results.length > 0 && <SearchResultsList results={results} />}
-      </div> */}
+        {results && results.length > 0 && (
+          <SearchResultsList results={results} />
+        )}
+      </div>
     </div>
   );
 };
