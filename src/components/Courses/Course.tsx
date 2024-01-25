a0674e0f7dfab054d48260bb0d471d9033f1ac6d
Nanashikung <nata.station@hotmail.com>
2024-01-25 14:34:04 +0700

    Static Course Detail Page

--- src/components/Courses/Course.tsx
+++ src/components/Courses/Course.tsx
@@ -1,113 +1,74 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import Navbar from "../Homepage/Navbar";
-import { ICourse } from "./props";
-import { fetchCourseData } from "./api";
+// import React, { useEffect, useState } from "react";
+// import Navbar from "../Homepage/Navbar";
+// import { ICourse } from "./props";
+// import { fetchCourseData } from "./api";
 
-export function Course(props: ICourse) {
-  const CoursePreview: React.FC = () => 
-    // const { courseId } = useParams();
-    const [courseId, setId] = useState<any>("261200");
-    const [courseData, setCourseData] = useState<any>(null);
-    const [buttonData, setButtonData] = useState<any>("");
+// export function Course(props: ICourse) {
+//   // const [courseData, setCourseData] = useState<any>(null);
+//   // useEffect(() => {
+//   //   const courseId = props.match.params.courseId; // Assuming you are using React Router and course ID is part of the URL
+//   //   fetchCourseData(courseId)
+//   //     .then((data) => setCourseData(data))
+//   //     .catch((error) => console.error(error));
+//   // }, [props.match.params.courseId]);
 
-    useEffect(() => {
-      if (courseId) {
-        fetchCourseData(courseId)
-          .then((data) => setCourseData(data))
-          .catch((error) => console.error(error));
-      }
-    }, [courseId]);
-    console.log("ID is " + courseId);
-    console.log("courseData " + courseData);
-    if (!courseData) {
-      // Loading state
-      return <div> Loading... </div>;
-    }
+//   // if (!courseData) {
+//   //   // Loading state
+//   //   return <div>Loading...</div>;
+//   // }
 
-    return (
-      <div className="bg-black h-screen">
-        <Navbar />
+//   return (
+//     <div className="bg-black h-screen">
+//       <Navbar />
+//       console.log()
+//       <div className="grid grid-cols-2 bg-[#fff4f4] text-2xl p-16 gap-8 h-full">
+//         {/* course detail section */}
+//         <div className=" ">
+//           {/* course No */}
+//           <div className="mb-4  ">
+//             {/* <span className="font-bold text-5xl">{courseData.id}</span> */}
+//             <span className="font-bold text-5xl">261200</span>
 
-        <div className="grid grid-cols-2 bg-[#fff4f4] text-2xl p-16 gap-8 h-full">
-          {/* course detail section */}
-          <div className=" ">
-            {/* course No */}
-            <div className="mb-4  ">
-              {/* <span className="font-bold text-5xl">{courseData.id}</span> */}
-              <span className="font-bold text-5xl">{courseData.id}</span>
+//             <span className="inline-block">
+//               <img
+//                 alt="Icon"
+//                 src="../src/components/Homepage/icon/image%2015.png"
+//                 className="ml-4 w-15 h-9"
+//               />
+//             </span>
+//           </div>
 
-              <span className="inline-block">
-                <img
-                  alt="Icon"
-                  src="../src/components/Homepage/icon/image%2015.png"
-                  className="ml-4 w-15 h-9"
-                />
-              </span>
-            </div>
-
-            {/* Name */}
-            <div className="font-bold text-4xl my-4">
-              {courseData.name_en} - {courseData.name_th}
-            </div>
-            {/* description */}
-            <div className="text-xl my-4">{courseData.description}</div>
-            <div className="font-bold text-4xl my-4">
-              Grade Visualize
-              <p className="font-normal my-4 text-sm">wait for api</p>
-            </div>
-            <div className="grid grid-flow-col-dense my-4">
-              <button
-                className=" rounded-full hover:bg-sky-200"
-                onClick={() =>
-                  setButtonData("Lecturer: " + courseData.lecturers)
-                }
-              >
-                Lecturer
-              </button>
-              <button
-                className=" rounded-full hover:bg-sky-200"
-                onClick={() =>
-                  setButtonData("Location: " + courseData.location)
-                }
-              >
-                Location
-              </button>
-              <button
-                className=" rounded-full hover:bg-sky-200"
-                onClick={() =>
-                  setButtonData("Schedule: " + courseData.schedule)
-                }
-              >
-                Schedule
-              </button>
-              <button
-                className=" rounded-full hover:bg-sky-200"
-                onClick={() => setButtonData("Room: " + courseData.rooms)}
-              >
-                Room
-              </button>
-            </div>
-            <div className="text-xl">
-              {courseData ? (
-                <div>
-                  <p>{buttonData}</p>
-                </div>
-              ) : (
-                "error"
-              )}
-            </div>
-          </div>
-          {/* review section */}
-          <div className="">
-            <div>Review</div>
-            <div className="rounded-3xl bg-white p-4 text-xl h-full">
-              fetch data from DB
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  };
-  // export default CoursePreview;
-}
+//           {/* Name */}
+//           <div className="font-bold text-4xl my-4">
+//             The Life Long - ชีวิตยืดยาว
+//           </div>
+//           {/* description */}
+//           <div className="text-xl my-4">
+//             การศึกษาเกี่ยวกับธรรมชาติและสิ่งแวดล้อมโดยตรงมีความหลากหลายและลึกซึ้งมากมาย
+//             นอกจากภูมิอากาศ, นิวเคลียร์, การจัดการทรัพ
+//             ยากรธรรมชาติ,และการป้องกันมลพิษที่เป็นส่วนสำคัญแล้วยังมีหลายหัวข้ออื่นที่เกี่ยวข้อง
+//             เช่น การประเมินผลกระทบสิ่งแวดล้อม
+//           </div>
+//           <div className="font-bold text-4xl my-4">
+//             Grade Visualize
+//             <p className="font-normal my-4 text-sm">wait for api</p>
+//           </div>
+//           <div className="grid grid-flow-col-dense my-4">
+//             <button className=" rounded-full hover:bg-sky-200">Lecturer</button>
+//             <button className=" rounded-full hover:bg-sky-200">Location</button>
+//             <button className=" rounded-full hover:bg-sky-200">Schedule</button>
+//             <button className=" rounded-full hover:bg-sky-200">Room</button>
+//           </div>
+//           <div className="text-sm">wait for api</div>
+//         </div>
+//         {/* review section */}
+//         <div className="">
+//           <div>Review</div>
+//           <div className="rounded-3xl bg-white p-4 text-xl h-full">
+//             fetch data from DB
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
--- src/routes/previews.tsx
+++ src/routes/previews.tsx
@@ -1,40 +1,20 @@
 import { RouteObject } from "react-router-dom";
 import CoursePreview from "../components/Courses/preview";
+import Review from "../components/Reviews/Review";
 import Submited from "../components/Reviews/Submited";
-import ReviewPage from "../components/Reviews/ReviewPage";
-import ReviewPopup from "../components/Reviews/ReviewPopup";
-import GetCourses from "../components/Reviews/CourseInfoDB/GetCourses";
 
 const routes: RouteObject[] = [
   {
-    path: "/courses/courseId", // Dynamic route for course ID
+    path: "/courses/:courseId", // Dynamic route for course ID
     element: <CoursePreview />,
   },
   {
     path: "/reviews/review",
-    element: <ReviewPage />,
-  },
-  {
-    path: "/reviews/reviewpopup",
-    element: (
-      <div style={{ width: 1910, height: 980 }}>
-        <div style={{ height: 110 }}></div>
-        <div className="flex justify-center">
-          <ReviewPopup
-            courseNo="207115-001"
-            courseName="Physics Laboratory for Engineering and Agro-Industry Students 1"
-          />
-        </div>
-      </div>
-    ),
+    element: <Review />,
   },
   {
     path: "/reviews/submited",
     element: <Submited />,
-  },
-  {
-    path: "/reviews/getcourseinfo",
-    element: <GetCourses />,
   },
 ];
 
