cb06307cc22a535ab419bd5f13f953ec1be50d19
AndaRaiwin <andaraiwin@gmail.com>
2024-02-02 17:35:10 +0700

    change text color

--- src/components/Courses/Comments.tsx
+++ src/components/Courses/Comments.tsx
@@ -22,12 +22,12 @@ const comments: React.FC = () => {
             {reviews.map(review =>
                 <div> {review.content.replace(RegexWhiteSpacePattern, "") === "" || review.content === "\n"? null:
                     <div className=" p-[5px] mb-4 flex justify-between border-b-[1px] border-black pb-5">
-                        <div> {review.content} </div>
-                        <div className="flex flex-col justify-items-center">
+                        <div> Comment: <br/> {review.content} <br/> Environment: <br/> {review.classroom_environment} </div>
+                        <div className="flex flex-col justify-items-center text-orange-300">
                             <div className="text-center"> Rating: {review.rating} </div>
-                            <div className="text-center"> Year: {`${review.semester} / ${review.year}`}</div>
+                            <div className="text-center"> Year: {`${review.semester}/${review.year}`}</div>
                             <div className="text-center">
-                                {review.grading_method.length === 2 ? review.grading_method[0] + " | " + review.grading_method[1] : review.grading_method.length === 1 ? review.grading_method[0] : "none"}
+                                {review.grading_method.length === 2 ? review.grading_method[0] + " & " + review.grading_method[1] : review.grading_method.length === 1 ? review.grading_method[0] : "none"}
                             </div>
                         </div>
                     </div>}
