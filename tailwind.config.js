72c386cc6da78c39efed02d1deb0c8e76d0a0453
Panyapat Wongdee <panyapat12345@gmail.com>
2024-03-07 15:03:43 +0700

    debug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {
-            fontFamily: {
-                kanit: ["Kanit"],
-            },
-        },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        kanit: ["Kanit"],
+      },
     },
-    plugins: [
-      require('flowbite/plugin')
-    ],
+  },
+  //   plugins: [require("flowbite/plugin")],
+  plugins: [],
 };
-
